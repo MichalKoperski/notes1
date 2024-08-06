d4467c4a047a92bd8813cc175843dc3dd0b19fbf
michalkoperski <michal@koperski.cc>
2024-08-06 18:52:34 +0200

    1

---
+++ 27.kwargs.py
@@ -0,0 +1,9 @@
+# **kwargs = parameter that will pack all arguments into a dictionary
+#            useful so that a function can accept a varying amount of keyword arguments
+
+def hello(**names):
+    print("Hello",end=" ")
+    for key,value in names.items():
+        print(value,end=" ")
+
+hello(title="Mr.",first="Bro",middle="Dude",last="Code")
