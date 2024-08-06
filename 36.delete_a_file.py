8d254aae5242a17b0359adc2ca91f963e86565e2
michalkoperski <michal@koperski.cc>
2024-08-06 21:28:09 +0200

    1

---
+++ 36.delete_a_file.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+path = '36.test.txt'
+
+try:
+    os.remove(path)     #delete a file
+    os.rmdir(path)      #delete a file or empty folder
+    shutil.rmtree(path) #delete files and/or folders
+except FileNotFoundError:
+    print("That file was not found")
+except PermissionError:
+    print("You do not have permission to delete that")
+except OSError:
+    print("That folder contains files")
+else:
+    print(path+" was deleted")
---
+++ 37.modules.py
@@ -0,0 +1,9 @@
+import modules_messages as msg
+
+msg.hello()
+msg.bye()
+
+from modules_messages import hello,bye
+
+hello()
+bye()
